952871d9c0d996409c6d7bd31a065cd6c9daea6d
manzitresor <manzitre@gmail.com>
2025-08-08 14:03:39 +0200

    chore: implement routing functionality

---
+++ src/routes/index.tsx
@@ -0,0 +1,15 @@
+import HomePage from "@/pages/HomePage";
+import Login from "@/pages/Login";
+import { createBrowserRouter } from "react-router-dom";
+
+
+export const router = createBrowserRouter([
+    {
+            path: "/",
+            element: <HomePage />,
+    },
+    {
+        path: '/login',
+        element: <Login/>
+    }
+])
