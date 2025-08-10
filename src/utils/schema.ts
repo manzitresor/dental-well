1609f450e6b10c7bb47599d57733035073928539
manzitresor <manzitre@gmail.com>
2025-08-10 20:09:52 +0200

    chore: add signup schema

--- src/utils/schema.ts
+++ src/utils/schema.ts
@@ -4,3 +4,14 @@ export const loginSchema = z.object({
   email: z.string().min(1, "Email is required").email("Please enter a valid email address"),
   password: z.string().min(6, "Password must be at least 6 characters").min(1, "Password is required"),
 })
+
+export const singupSchema = z.object({
+    fullName: z.string().min(1, "Full name is required"),
+    email: z.string().min(1, "Email is required").email("Please enter a valid email address"),
+    phoneNumber: z.string().min(1, "Phone number is required").regex(/^\d{10}$/, "Phone number must be 10 digits"),
+    password: z.string().min(6, "Password must be at least 6 characters").min(1, "Password is required"),
+    confirmPassword: z.string().min(1, "Confirm password is required"),
+}).refine((data) => data.password === data.confirmPassword,{
+  message: "Passwords do not match",
+  path: ["confirmPassword"],
+})
