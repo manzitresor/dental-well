95ce468ec4243dd9f4cac47517a5dc9367699418
manzitresor <manzitre@gmail.com>
2025-08-13 03:55:24 +0200

    chore: add login slice implementation

---
+++ src/lib/api.ts
@@ -0,0 +1,24 @@
+import axios from 'axios'
+import { API_URL } from "../constants"
+
+const api = axios.create({
+    baseURL: `${API_URL}`,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+})
+
+api.interceptors.request.use(
+    async config => {
+        const token = localStorage.getItem('token')
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`
+        }
+        return config
+    },
+    error => {
+        return Promise.reject(error)
+    },
+)
+
+export default api
