8791ad5473353825fafa558e26860c3d947b859c
manzitresor <manzitre@gmail.com>
2025-08-19 01:23:38 +0200

    chore: add function to get status color

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -7,3 +7,18 @@ export const handleAxiosError = (error: AxiosError) => {
         return error.message
     }
 }
+
+ export const getStatusColor = (status: string) => {
+    switch (status) {
+      case "Scheduled":
+        return "bg-yellow-100 text-yellow-800"
+      case "Confirmed":
+        return "bg-green-100 text-green-800"
+      case "Completed":
+        return "bg-blue-100 text-blue-800"
+      case "Cancelled":
+        return "bg-red-100 text-red-800"
+      default:
+        return "bg-gray-100 text-gray-800"
+    }
+  }
